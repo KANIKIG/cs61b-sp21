f197a6cd6940d485c815eb26362a1aec211d9609
KANIKIG <gdc3489@gmail.com>
2022-03-04 21:53:58 +0800

    Update fix lab2/prime

--- lab2/IntList/IntListExercises.java
+++ lab2/IntList/IntListExercises.java
@@ -77,6 +77,6 @@ public class IntListExercises {
             lst.first *= lst.first;
         }
 
-        return currElemIsPrime || squarePrimes(lst.rest);
+        return squarePrimes(lst.rest) || currElemIsPrime;
     }
 }
