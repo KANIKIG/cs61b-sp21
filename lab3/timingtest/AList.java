bb41e7fe796805c6877167cad0e584d49b72ce6d
KANIKIG <gdc3489@gmail.com>
2022-04-05 22:41:45 +0800

    Upadate lab3

--- lab3/timingtest/AList.java
+++ lab3/timingtest/AList.java
@@ -34,7 +34,7 @@ public class AList<Item> {
     /** Inserts X into the back of the list. */
     public void addLast(Item x) {
         if (size == items.length) {
-            resize((int) (size * 1.01));
+            resize((int) (items.length * 2));
         }
 
         items[size] = x;
