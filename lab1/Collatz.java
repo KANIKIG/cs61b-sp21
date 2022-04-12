f7bbcb4df1620e5eb497c7ee699e66810cf78eb4
mac <mac@macdeMacBook-Pro.local>
2022-04-12 13:48:38 +0800

    Finish Lab4 merge

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -5,12 +5,21 @@ public class Collatz {
 
     /** Buggy implementation of nextNumber! */
     public static int nextNumber(int n) {
-        if (n % 2 == 1) {
+        if (n  == 128) {
+            return 1;
+        } else if (n == 5) {
             return 3 * n + 1;
         } else {
-            return n / 2;
+            return n * 2;
         }
     }
+    // public static int nextNumber(int n) {
+    //     if (n % 2 == 1) {
+    //         return 3 * n + 1;
+    //     } else {
+    //         return n / 2;
+    //     }
+    // }
 
     public static void main(String[] args) {
         int n = 5;
