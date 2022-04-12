ba14f28e03d80838d4020fa3b16f032341bfa4ee
mac <mac@macdeMacBook-Pro.local>
2022-04-12 16:50:22 +0800

    Add 增加了取消回滚的方法

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -5,21 +5,8 @@ public class Collatz {
 
     /** Buggy implementation of nextNumber! */
     public static int nextNumber(int n) {
-        if (n  == 128) {
-            return 1;
-        } else if (n == 5) {
-            return 3 * n + 1;
-        } else {
-            return n * 2;
-        }
+        return n % 2 == 0 ? n/2 : 3*n + 1;
     }
-    // public static int nextNumber(int n) {
-    //     if (n % 2 == 1) {
-    //         return 3 * n + 1;
-    //     } else {
-    //         return n / 2;
-    //     }
-    // }
 
     public static void main(String[] args) {
         int n = 5;
